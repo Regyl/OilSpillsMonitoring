74618b38309dc23d89aa4fe1ee1e952f6f33ca1d
Regyl <novikovevgeny1@gmail.com>
2021-12-02 16:50:57 +0300

    initial commit

---
+++ src/main/java/com/rosatom/oilspillmonitoring/OilSpillMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.rosatom.oilspillmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilSpillMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OilSpillMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rosatom/oilspillmonitoring/OilSpillMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosatom.oilspillmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilSpillMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
