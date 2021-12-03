436ffc9e201d44858a0c4569d48a072845d82a3c
Regyl <novikovevgeny1@gmail.com>
2021-12-03 21:03:01 +0300

    finish dowload file

--- src/main/java/com/rosatom/oilspills/api/controller/PollutionController.java
+++ src/main/java/com/rosatom/oilspills/api/controller/PollutionController.java
@@ -3,17 +3,30 @@ package com.rosatom.oilspills.api.controller;
 import com.rosatom.oilspills.api.controller.dto.request.PollutionDto;
 import com.rosatom.oilspills.api.controller.dto.response.PollutionDtoResponse;
 import com.rosatom.oilspills.api.mapper.PollutionMapper;
+import com.rosatom.oilspills.entity.Location;
 import com.rosatom.oilspills.entity.Pollution;
+import com.rosatom.oilspills.service.LocationService;
 import com.rosatom.oilspills.service.PollutionService;
+import com.rosatom.oilspills.service.UavRouteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.net.MalformedURLException;
 import java.util.UUID;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Tag(name = "Pollutions")
 
@@ -23,10 +36,15 @@ public class PollutionController {
 
     private final PollutionService service;
     private final PollutionMapper mapper;
+    private final UavRouteService uavRouteService;
+    private final LocationService locationService;
 
-    public PollutionController(PollutionService service, PollutionMapper mapper) {
+    public PollutionController(PollutionService service, PollutionMapper mapper,
+                               UavRouteService uavRouteService, LocationService locationService) {
         this.service = service;
         this.mapper = mapper;
+        this.uavRouteService = uavRouteService;
+        this.locationService = locationService;
     }
 
     @GetMapping("/")
@@ -41,8 +59,9 @@ public class PollutionController {
     @Operation(summary = "Create record about pollution")
     public Mono<PollutionDtoResponse> save(@RequestBody @Valid PollutionDto dto) {
         Pollution pollution = mapper.toEntity(dto);
-        return service.save(pollution)
-                .map(mapper::toDto);
+        return uavRouteService.generate(dto.getStartLocation(), dto.getEndLocation())
+                .publish(p -> service.save(pollution).map(mapper::toDto));
+
     }
 
     @DeleteMapping("/{id}")
@@ -50,4 +69,11 @@ public class PollutionController {
     public Mono<Void> deleteById(@PathVariable UUID id) {
         return service.deleteById(id);
     }
+
+    @GetMapping("/files")
+    @Operation(summary = "Download file with pollutions")
+    public Mono<Resource> download() {
+        return Mono.just(new ClassPathResource("top50.geojson"));
+    }
+
 }
