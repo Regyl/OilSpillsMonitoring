eb13da0f1cfdc08a19a28c4fd9482a7386787e55
Regyl <novikovevgeny1@gmail.com>
2021-12-04 05:00:05 +0300

    finish tracks for uav

--- src/main/java/com/rosatom/oilspills/service/UavRouteService.java
+++ src/main/java/com/rosatom/oilspills/service/UavRouteService.java
@@ -6,6 +6,7 @@ import com.rosatom.oilspills.repository.UavRouteLocationRepository;
 import com.rosatom.oilspills.repository.UavRouteRepository;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.Yaml;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -44,26 +45,25 @@ public class UavRouteService {
         UavRoute uavRoute = new UavRoute();
         uavRoute.setFlightAltitude(1000L);
         Mono<UavRoute> uavRouteMono = repository.save(uavRoute);
-        UavRouteLocation startLoc = new UavRouteLocation(startLocId);
-        UavRouteLocation endLoc = new UavRouteLocation(endLocId);
 
-        return uavRouteMono.map(entity -> {
-            startLoc.setUavRouteId(entity.getId());
-            endLoc.setUavRouteId(entity.getId());
+
+        return uavRouteMono.subscribe(entity -> {
+            UavRouteLocation startLoc = new UavRouteLocation(entity.getId(), startLocId);
+            UavRouteLocation endLoc = new UavRouteLocation(entity.getId(), endLocId);
+
 
             uavRouteLocationRepository.save(startLoc)
-                    .map(location -> {
-                        entity.addLocation(location);
-                        return entity;
-                    }).subscribe();
+                    .subscribe(entity::addLocation);
+
+            locationService.getMiddlePoint(startLocId, endLocId).subscribe(id -> {
+                UavRouteLocation middleLoc = new UavRouteLocation(id, entity.getId());
+                uavRouteLocationRepository.save(middleLoc)
+                        .subscribe(entity::addLocation);
+            });
+
 
             uavRouteLocationRepository.save(endLoc)
-                    .map(location -> {
-                        log.info(location.getUavRouteId().toString());
-                        entity.addLocation(location);
-                        return entity;
-                    }).subscribe();
-            return entity;
-        }).subscribe();
+                    .subscribe(entity::addLocation);
+        });
     }
 }
